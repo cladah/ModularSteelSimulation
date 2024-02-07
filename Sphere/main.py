1beeb3d0b6dd8b026e20d4349b579f738beb8d0a
cladah <122968031+cladah@users.noreply.github.com>
2024-02-07 13:29:36 +0100

    Adding looping

--- Sphere/main.py
+++ Sphere/main.py
@@ -24,6 +24,39 @@ def GUI():
     app.mainloop()
     removedatastreamcache()
 
+def looping():
+    setupSimulation()
+    differentin = [["Material", "Composition", {"C": 0.2,"N": 0.025,"Cr": 1.6,"Mn": 0.5,"Ni": 1.5,"Mo": 0.3,"Si": 0.2}],
+                   ["Material", "Composition", {"C": 0.5,"N": 0.025,"Cr": 1.6,"Mn": 0.5,"Ni": 1.5,"Mo": 0.3,"Si": 0.2}],
+                   ["Material", "Composition", {"C": 1.0,"N": 0.025,"Cr": 1.6,"Mn": 0.5,"Ni": 1.5,"Mo": 0.3,"Si": 0.2}]]
+    saveloc = ["Test1.xmdf", "Test2.xmdf", "Test3.xmdf"]
+    i = 0
+    for a in differentin:
+        change_input(*a)
+        data = read_input()
+        createdatastreamcache(data["Datastream"]["Cachedirect"])
+
+        # resetdatastream()
+        createinputcache()
+
+        modules = list()
+        modules.append(Meshingmodule())
+        modules.append(Carbonitridingmodule())
+        modules.append(TTTdiagrammodule())
+        modules.append(Transformationmodelmodule())
+        modules.append(Quenchingmodule())
+
+        for currentmodule in modules:
+            if currentmodule.modulename() != "Meshing":
+                tid = threading.Thread(target=run_single_module, args=(currentmodule,))
+                tid.start()
+                progressmonitor(tid, currentmodule)  # Making sure thread is done
+            else:
+                run_single_module(currentmodule)
+
+        removedatastreamcache()
+        savedatastream(saveloc[i])
+        i = i + 1
 
 def modelling():
     setupSimulation()
